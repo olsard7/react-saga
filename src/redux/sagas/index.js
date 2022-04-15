0194c8aa9d261a9dad7e262a52fb0c403c17ec7e
Oleg <olsard7@gmail.com>
2022-04-15 14:45:48 +0300

    add watcher - Watch click

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,4 +1,15 @@
+import { take } from "@redux-saga/core/effects";
+import { INCREASE_COUNTER, DECREASE_COUNTER } from "../actionTypes";
+
+export function* workerSaga() {}
+
+export function* watchClickSaga() {
+  yield take(INCREASE_COUNTER);
+  console.log("increase");
+  yield take(DECREASE_COUNTER);
+  console.log("decrease");
+}
+
 export default function* rootSaga() {
-  console.log("Saga works");
-  yield;
+  yield watchClickSaga();
 }
